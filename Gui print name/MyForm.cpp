813258ba6c01e0a64cc0d4b81baf1c0a71d1afaf
Lazar Alexandru <alex.lazarr.al@gmail.com>
2024-04-23 15:47:32 +0300

    Add project files.

---
+++ Gui print name/MyForm.cpp
@@ -0,0 +1,13 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles;
+	Application::SetCompatibleTextRenderingDefault(false);
+	Guiprintname::MyForm form;
+	Application::Run(% form);
+
+
+}
